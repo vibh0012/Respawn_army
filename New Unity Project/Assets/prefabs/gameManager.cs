01e2fd1f00d2a9c6725c0be1d360e84f0d9efbfc
vibhor <vibhor@vibhor.com>
2018-12-19 12:54:06 +0530

    adding initial commit

---
+++ New Unity Project/Assets/prefabs/gameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameManager : MonoBehaviour {
+
+	GameObject[] spawns;
+	public GameObject enemy;
+	public static int count = 0;
+	// Use this for initialization
+	void Start () {
+		spawns = GameObject.FindGameObjectsWithTag ("spawn");
+		InvokeRepeating ("spawnNow", 3,3);
+	}
+	
+	// Update is called once per frame
+	void spawnNow () {
+		//if (count < 5) {
+			count++;
+			GameObject.Instantiate (enemy, spawns [Random.Range (0, spawns.Length - 1)].transform.position, Quaternion.identity);
+		//}
+	}
+}
---
+++ New Unity Project/Assets/prefabs/playerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour {
+
+	public GameObject target;
+	public float speed = 4;
+	Animator playerAnim;
+	// Use this for initialization
+	void Start () {
+		target = Camera.main.gameObject;
+		playerAnim = gameObject.GetComponent <Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ((gameObject.transform.position - target.transform.position).sqrMagnitude < 10) {
+			playerAnim.SetTrigger ("idle");
+			return;
+		}
+		gameObject.transform.LookAt (target.transform);
+		gameObject.transform.position = Vector3.MoveTowards (gameObject.transform.position, target.transform.position, Time.deltaTime * speed);
+	}
+
+	void OnCollisionEnter(Collision col){
+
+		if (col.gameObject.tag.Equals ("sword")) {
+			playerAnim.SetTrigger ("isDead");
+			gameManager.count--;
+			Invoke ("die", 3);
+		}
+	}
+
+	void die(){
+		
+		Destroy (gameObject);
+	}
+}
